8cf3f644dcc79f8352c879c4b95cd90f4755e615
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-09-13 13:42:38 -0400

    some changes

--- src/components/thing.js
+++ src/components/thing.js
@@ -3,9 +3,11 @@ class Thing {
     this.title = thingJSON.title
     this.id = thingJSON.id
     this.kind = thingJSON.kind
+    this.genre = thingJSON.genre
+    this.creator = thingJSON.creator
   }
 
-  render() {
-    return `<li data-thingid='${this.id}' data-props='${JSON.stringify(this)}' class='thing-element'>${this.title} <i data-action='delete-thing' class="em em-scream_cat"></i></li>`
-  }
+  // render() {
+  //   return `<li data-thingid='${this.id}' data-props='${JSON.stringify(this)}' class='thing-element'>${this.title} <i data-action='delete-thing' class="em em-scream_cat"></i></li>`
+  // }
 }
--- src/components/things.js
+++ src/components/things.js
@@ -1,74 +1,127 @@
 class Things {
   constructor() {
+    //console.log(this)
     this.things = []
     this.initBindingsAndEventListeners()
     this.adapter = new ThingsAdapter()
     this.fetchAndLoadThings()
+    
   }
 
+
   initBindingsAndEventListeners() {
+    //console.log(this)
     this.thingsForm = document.getElementById('new-thing-form')
-    this.thingInput = document.getElementById('new-thing-body')
+    this.thingInput = document.getElementById('new-thing-title')
     this.thingsNode = document.getElementById('things-container')
     this.songsNode = document.getElementById('songs-container')
     this.podcastsNode = document.getElementById('podcasts-container')
-    this.thingsForm.addEventListener('submit',this.handleAddThing.bind(this))
-    this.thingsNode.addEventListener('click',this.handleDeleteThing.bind(this))
+    //this.thingsForm.addEventListener('submit',this.handleAddThing.bind(this))
+    //this.thingsNode.addEventListener('click',this.handleDeleteThing.bind(this))
+    this.thingInput.addEventListener('keyup', this.filterThings.bind(this))
   }
+
+
+
+  filterThings(){
+    const searchInput = event.target.value
+
+    console.log(this.things)
+    //let foundArr = []
+    
+    
+    const filteredArray = this.things.filter(function(instance){
+        
+        for(var key in instance){
+              if(instance[key].toString().includes(searchInput)){
+                  //console.log(instance[key])
+                  //foundArr.push(instance)
+                  return instance
+              }  
+            }
+            // return foundArr
+
+        })
+      
+
+      // const htmlTemplate = filteredArray.map(function(instance){
+
+      // 
+      // debugger
+      renderFilteredArray(filteredArray)
+
+
+    }
+
+    
+    
+    
+    
+
+    //this = whatever we typed in our form because we bound it on line 19
+    
+
+
+
 
   fetchAndLoadThings() {
+    
     this.adapter.getThings()
     .then( thingsJSON => thingsJSON.forEach( thing => this.things.push( new Thing(thing) )))
-      .then( this.render.bind(this) )
-      .catch( () => alert('The server does not appear to be running') )
+      // .then( this.render.bind(this) )
+      .catch( (e) => console.log(e) )
+      //console.log(this)
   }
 
-  handleAddThing() {
-    event.preventDefault()
-    const body = this.thingInput.value
-    this.adapter.createThing(body)
-    .then( (thingJSON) => this.things.push(new Thing(thingJSON)) )
-    .then(  this.render.bind(this) )
-    .then( () => this.thingInput.value = '' )
-  }
+  // handleAddThing() {
+  //   //console.log(this)
+  //   event.preventDefault()
+  //   const title = this.thingInput.value
+  //   this.adapter.createThing(title)
+  //   .then( (thingJSON) => this.things.push(new Thing(thingJSON)) )
+  //   .then(  this.render.bind(this) )
+  //   .then( () => this.thingInput.value = '' )
+  // }
 
-  handleDeleteThing() {
-    if (event.target.dataset.action === 'delete-thing' && event.target.parentElement.classList.contains("thing-element")) {
-      const thingId = event.target.parentElement.dataset.thingid
-      this.adapter.deleteThing(thingId)
-      .then( resp => this.removeDeletedThing(resp) )
-    }
-  }
-
-  removeDeletedThing(deleteResponse) {
-    this.things = this.things.filter( thing => thing.id !== deleteResponse.thingId )
-    this.render()
-  }
-
-  thingsHTML() {
-    return this.things.map( thing => thing.render() ).join('')
-  }
-
-  render() {
-  //   debugger
-  //   if(this.kind === "podcast"){
-  //     this.podcastsNode.innerHTML = `<ul>${this.thingsHTML()}</ul>`
-  //   } else if (this.kind === "song"){
-  //     this.songsNode.innerHTML = `<ul>${this.thingsHTML()}</ul>`
-  //   } else {
-  //     this.thingsNode.innerHTML = `<ul>${this.thingsHTML()}</ul>`
+  // handleDeleteThing() {
+  //   if (event.target.dataset.action === 'delete-thing' && event.target.parentElement.classList.contains("thing-element")) {
+  //     const thingId = event.target.parentElement.dataset.thingid
+  //     this.adapter.deleteThing(thingId)
+  //     .then( resp => this.removeDeletedThing(resp) )
   //   }
-  //
-    let objs = this
-    this.things.filter(function(thing) {
+  //   //console.log(this)
+  // }
+
+  // removeDeletedThing(deleteResponse) {
+  //   this.things = this.things.filter( thing => thing.id !== deleteResponse.thingId )
+  //   this.render()
+  //   //console.log(this)
+  // }
+
+  // thingsHTML() {
+  //   //console.log(this)
+  //   return this.things.map( thing => thing.render() ).join('')
+  // }
+
+  // render() {
+  //   this.thingsNode.innerHTML = `<ul>${this.thingsHTML()}</ul>`
+  // }
+
+  renderFilteredArray(something) {
+    debugger
+    // let objs = this
+    //console.log(this)
+    something.map(function(thing) {
+    //console.log(this)
       if (thing.kind.includes("song")) {
-
         objs.songsNode.innerHTML = `<ul>${thing.title}</ul>`
-
-      } else {
+      } else if(thing.kind.includes("podcast")) {
         objs.podcastsNode.innerHTML = `<ul>${thing.title}</ul>`
+      } else {
+        objs.thingsNode.innerHTML = `<ul>${thing.title}</ul>`
       }
     })
+    // return a new string of HTML rendered to a certain point on the page
 
   }
 
